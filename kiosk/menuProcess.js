d8c4ccb889a21c6e713b656394f35863d6c38504
findwhatiwant <jws0041@naver.com>
2023-12-02 01:07:54 +0900

    update menuCount and Designs
    메뉴창의 헤더를 달고 위치를 조금 조정하였으나 동적인 크기값이 아닌 상수로 지정한 값으로 해상도에 따라 의도치 않게 변형될 수 있으나 이후에 변경할 예정입니다. 그리고 메뉴를 클릭할 때 총 금액이 옆에 뜨게 됩니다.

--- kiosk/menuProcess.js
+++ kiosk/menuProcess.js
@@ -1,4 +1,15 @@
-function menuClick(menuName) {
-    document.getElementById("menuResults").innerHTML = document.getElementById("menuResults").innerHTML + menuName + "<br>";
+var menuCount = [0,0,0,0,0,0];
 
+var priceResult = 0;
+
+function priceUpdate(){
+    document.getElementById("priceResult").innerHTML = priceResult;
 }
+
+function menuClick(menuId) {
+    document.getElementById("menuResults").innerHTML = document.getElementById("menuResults").innerHTML + menuId.menuName + "<br>";
+    priceResult += menuId.price;
+    priceUpdate();
+    menuCount[menuId.number] += 1;
+}
+
