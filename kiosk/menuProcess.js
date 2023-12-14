4b60234920c04f66bfcc8d200dfa9e73f5d48af7
findwhatiwant <jws0041@naver.com>
2023-12-14 16:55:08 +0900

    결제화면 구현

--- kiosk/menuProcess.js
+++ kiosk/menuProcess.js
@@ -11,10 +11,40 @@ function menuClick(menuId) {
     priceResult += menuId.price;
     priceUpdate();
     menuCount[menuId.number] += 1;
+    console.log(menuCount);
+}
+
+function menuNameReturn(ipt){
+    for(var i=0; i<menuCount.length; i++){
+        if(Object.values(menu)[i].number == ipt){
+            return Object.values(menu)[i].menuName;
+        }
+    }
 }
 
 function paymentClick(){
     const background = document.getElementsByClassName("beforeofdimmed")[0].className = "dimmed";
+    let popupTag = document.createElement('div');
+    let popupMenu = document.createElement('div');
+    let popupButton = document.createElement('button');
+    
+    popupMenu.className = "popupMenu";
 
+    popupTag.className = "popup";
+
+    popupButton.id = "finalPaymentButton";
+    popupButton.innerHTML = "결제하기";
+    popupButton.onclick = "";
+
+    popupTag.innerHTML = "<div class='title'>결제 화면</div><div class='content'></div>";
+    for(var i=0; i<menuCount.length; i++){
+        if(menuCount[i] > 0){
+            popupMenu.innerHTML += menuNameReturn(i)+ " " + menuCount[i] + "개" + "<br>";
+        }
+    }
+    popupTag.appendChild(popupMenu);
+    popupTag.appendChild(popupButton);
+    document.getElementById("popupDiv").appendChild(popupTag);
+    
 }
 
